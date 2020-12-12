17613f98a855b99698160501018826568c86d913
Abhishek <abhimore74@gmail.com>
2020-12-12 16:39:12 -0500

    new project structure

--- index.js
+++ index.js
@@ -9,51 +9,27 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-db.defaults({projects: []}).write()
+db.defaults({default: "", projects: []}).write()
+console.log()
 
-function usage() {
-
-    const usageText = `
-        todo helps you manage you todo tasks.
-    
-        usage:
-        todo <command>
-    
-        commands can be:
-    
-        new:      used to create a new todo
-        get:      used to retrieve your todos
-        complete: used to mark a todo as complete
-        help:     used to print the usage guide
-    `
-
-    console.log(usageText)
+function getTasks() {
+    let name = db.get('default').value()
+    let project = db.get('projects').find({name: name})
+    tasks = project.get('tasks').map("title").value()
+    if (tasks.length == 0) {
+        err = chalk.red("default project not found\ntry creating one with 'todo np'")
+        console.log(err)  
+    } else {
+        name = chalk.magentaBright(name)
+        console.log(name)
+    }
 }
 
-function errorLog(error) {
-    const err = chalk.red(error)
-    console.log(err)
-}
-
-function newProject() {
-    inquirer.prompt([{
-        type: "input",
-        name: "project",
-        message: "project name?"
-    }]).then(name => {
-        projName = name['project']
-        db.get('projects').push({
-            name: projName,
-            tasks: []
-        }).write()
-    })
-}
-
-function newTask() {
+function createProject() {
     inquirer.prompt([{
         type: "input",
         name: "task",
-        message: "task name?"
+        message: "project name?"
     }]).then(name => {
         taskName = name['project']
         db.get('tasks').push({
@@ -63,54 +39,13 @@ function newTask() {
     })
 }
 
-function chooseType() {
-    inquirer.prompt([{
-        type: "list",
-        name: "type",
-        message: "type?",
-        choices: ["task", "project", "cancel"]
-    }]).then(type => {
-        type = type["type"]
-        if(type == "project") {
-            newProject()
-        } else if (type == "task") {
-            newTask()
-        } else {
-            close()
-        }
-    })
-}
-
-function getProjList() {
-    const projects = db.get('projects').value()
-    console.log()
-    projects.forEach(project => {
-        name = chalk.magentaBright(project["name"])
-        console.log('\t' + name)
-    });
-}
-
-if (args.length > 3) {
-    errorLog("only one argument can be accepted")
-    usage()
-}
-
 switch(args[2]) {
     case undefined:
-        getProjList()
+        getTasks()
         break
-    case 'help':
-        usage()
-        break
-    case 'new':
-        chooseType()
-        break
-    case 'ls':
-        getProjList()
-        break
-    case 'complete':
+    case "np":
+        createProject()
         break
     default:
-        errorLog('invalid command passed')
-        usage()
+        break
 }
