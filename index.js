be6d613894d77758eacf503210293e02d822fc46
Abhishek <abhimore74@gmail.com>
2020-12-12 18:53:34 -0500

    project and task add

--- index.js
+++ index.js
@@ -13,29 +13,46 @@ db.defaults({default: "", projects: []}).write()
 console.log()
 
 function projectExists(name) {
-    project = db.get('projects').find({name: name})
-
-    if (project) {
-        return true
+    project = db.get('projects').find({name: name}).value()
+    if (project == undefined) {
+        return false
     }
-    return false
+    return true
 }
 
 function getTasks() {
     let name = db.get('default').value()
     let project = db.get('projects').find({name: name})
     tasks = project.get('tasks').map("title").value()
-    if (tasks.length == 0) {
-        err = chalk.red("default project not found\ntry creating one with 'todo np'")
-        console.log(err)  
-    } else {
-        name = chalk.magentaBright(name)
-        console.log(name)
-    }
+
+    tasks.forEach(task => {
+        taskName = chalk.magentaBright(task)
+        console.log(taskName)
+    })
+}
+
+function addTask() {
+
+    let name = db.get('default').value()
+    let project = db.get('projects').find({name: name})
+
+    inquirer.prompt([{
+        type: "input",
+        name: "task",
+        message: "task "
+    }]).then(name => {
+        name = name["task"]
+        project.get("tasks").push({
+            title: name,
+            completed: false
+        }).write()
+    })
 }
 
 function createProject() {
     defaultProj = db.get('default').value()
+    let projName = ""
+
     inquirer.prompt([{
         type: "input",
         name: "project",
@@ -46,16 +63,42 @@ function createProject() {
             name: projName,
             tasks: []
         }).write()
-    })
 
+        if (!projectExists(defaultProj)) {
+            db.set('default', projName).write()
+        }
+
+        var success = chalk.green("project created!\nset a new default project with 'todo set'")
+        console.log(success)
+        
+    })
+}
+
+function setDefault() {
+    var projects = db.get('projects').map("name").value()
+
+    inquirer.prompt([{
+        type: "list",
+        name: "project",
+        choices: projects
+    }]).then(project => {
+        let name = project['project']
+        db.set('default', name).write()
+    })
 }
 
 switch(args[2]) {
     case undefined:
         getTasks()
         break
+    case "n":
+        addTask()
+        break
     case "np":
         createProject()
+        break
+    case "set":
+        setDefault()
         break
     default:
         break
